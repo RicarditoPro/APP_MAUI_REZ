5648341854e9aa7fe30c3784c61fd7cab9ecf28d
Ricardo Espejel Zarate <ricard.espejel.zarate@Outlook.com>
2023-09-13 16:04:39 -0600

    Metodos_POST_GET_DATOS

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("api/countries")]
+    public class CountriesController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context) 
+        {
+            this._context = context;
+        }
+
+        //METODO POST
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            _context.Add(country);
+            //_context.Countries.Add(country);
+
+            //para guardar el registro
+            await _context.SaveChangesAsync();
+            
+            //devuelve como fue llamado
+            return Ok(country);
+        }
+
+        //METODO GET
+        [HttpGet]
+
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+    }
+}
