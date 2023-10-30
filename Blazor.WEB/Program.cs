26c158a7c05f3ff27260ad01d385a3be6e185912
Ricardo Espejel Zarate <ricard.espejel.zarate@Outlook.com>
2023-10-30 17:22:48 -0600

    Versión06. Componentes, crear países, alertas SweetAlert2 Blazor.

--- Blazor.WEB/Program.cs
+++ Blazor.WEB/Program.cs
@@ -1,5 +1,6 @@
 using Blazor.WEB;
 using Blazor.WEB.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,14 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 /*Si el proyecto se conectara a Azure le colocaríamos la liga que nos proporcione Azure
   o la liga de cualquier otro Hosting del cual estemos consumiendo el servicio API.*/
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7188p/") });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7261/") });
-
-
-/*3 Formas de inyectar servicios por NET Core 
- -AddScoped Cuando necesesita crear una nueva instancias, genera objetos separados muy recomendable
- -AddTransient Paara inyectar solo una vex, empleado como alimentador de BD
- -AddSingleton Crea instancia de objeto y la reutiliza, muy vulnerable en seguridad(DataSensitive) y consume muhca memoria
-*/
-builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<IRepository,Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
--- Blazor.WEB/Repositories/IRepository.cs
+++ Blazor.WEB/Repositories/IRepository.cs
@@ -9,7 +9,21 @@ namespace Blazor.WEB.Repositories
 
         // 2 metodos 'Post' ya que aveces devuelve respuesta y aveces no
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        //POST que devuele respuesta
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+        //DELETE
+        Task<HttpResponseWrapper<object>> Delete(string url);
+
+        //PUT
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        //PUT QUE DEVUELVE RESPUESTA
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
+
+
+
 
     }
 }
